762a19eb317018a7dc547c8d6d532767cf85f787
andresrg99 <77860922+andresrg99@users.noreply.github.com>
2021-01-27 19:13:31 -0600

    Update Week 3 programming assignment.r

--- Week 3 programming assignment.r
+++ Week 3 programming assignment.r
@@ -1,3 +1,6 @@
+## I simply set the input x as a matrix
+## and then set the solved value "s" as a null
+## then I changed every reference to "mean" to "solve"
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL   
   evn <- environment()  
